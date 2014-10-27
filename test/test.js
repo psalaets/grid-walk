19d5da45f3a94dcfa483ea2aa47b3ad750131cfa
Paul Salaets <psalaets@gmail.com>
2014-10-27 16:03:11 -0400

    move helpers out of the way

--- test/test.js
+++ test/test.js
@@ -1,33 +1,6 @@
 var assert = require('chai').assert;
 var gw = require('../');
 
-// point
-function p(x, y) {
-  return {
-    x: x || 0,
-    y: y || 0
-  };
-}
-
-// coordinate
-function c(column, row) {
-  return {
-    column: column || 0,
-    row: row || 0
-  };
-}
-
-function createCollector() {
-  var coords = [];
-
-  function collector(coord) {
-    coords.push(coord)
-  }
-
-  collector.coords = coords;
-  return collector;
-}
-
 describe('walker#walk', function() {
   var walker, collector;
 
@@ -48,3 +21,30 @@ describe('walker#walk', function() {
     assert.deepEqual(collector.coords, [c(2, 2)]);
   });
 });
+
+// point in space
+function p(x, y) {
+  return {
+    x: x || 0,
+    y: y || 0
+  };
+}
+
+// coordinate in grid
+function c(column, row) {
+  return {
+    column: column || 0,
+    row: row || 0
+  };
+}
+
+function createCollector() {
+  var coords = [];
+
+  function collector(coord) {
+    coords.push(coord)
+  }
+
+  collector.coords = coords;
+  return collector;
+}

443cee4003e3cc9d04658b1be082a94d73a8cf6b
Paul Salaets <psalaets@gmail.com>
2014-10-27 16:14:30 -0400

    tests for vertical and horizontal cases

--- test/test.js
+++ test/test.js
@@ -20,6 +20,54 @@ describe('walker#walk', function() {
 
     assert.deepEqual(collector.coords, [c(2, 2)]);
   });
+
+  it('finds both cells when right-facing line crosses horizontally adjacent cells', function() {
+    var start = p(1, 11);
+    var end = p(22, 12);
+
+    walker.walk(start, end, collector);
+
+    assert.deepEqual(collector.coords, [
+      c(1, 2),
+      c(2, 2)
+    ]);
+  });
+
+  it('finds both cells when left-facing line crosses horizontally adjacent cells', function() {
+    var start = p(22, 11);
+    var end = p(1, 12);
+
+    walker.walk(start, end, collector);
+
+    assert.deepEqual(collector.coords, [
+      c(2, 2),
+      c(1, 2)
+    ]);
+  });
+
+  it('finds both cells when down-facing line cross vertically adjacent cells', function() {
+    var start = p(1, 1);
+    var end = p(1, 12);
+
+    walker.walk(start, end, collector);
+
+    assert.deepEqual(collector.coords, [
+      c(1, 1),
+      c(1, 2)
+    ]);
+  });
+
+  it('finds both cells when up-facing line cross vertically adjacent cells', function() {
+    var start = p(1, 11);
+    var end = p(1, 1);
+
+    walker.walk(start, end, collector);
+
+    assert.deepEqual(collector.coords, [
+      c(1, 2),
+      c(1, 1)
+    ]);
+  });
 });
 
 // point in space

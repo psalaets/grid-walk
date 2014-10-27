83b27eb440a95b3ffa2e2c462d6fb6e2a784bd46
Paul Salaets <psalaets@gmail.com>
2014-10-27 16:25:49 -0400

    re-nest tests

--- test/test.js
+++ test/test.js
@@ -21,77 +21,79 @@ describe('walker#walk', function() {
     assert.deepEqual(collector.coords, [c(2, 2)]);
   });
 
-  it('finds both cells when right-facing line crosses horizontally adjacent cells', function() {
-    var start = p(1, 11);
-    var end = p(22, 12);
+  describe('walking horizontal line', function() {
+    it('finds cells touched by right-facing line', function() {
+      var start = p(1, 11);
+      var end = p(22, 12);
 
-    walker.walk(start, end, collector);
+      walker.walk(start, end, collector);
 
-    assert.deepEqual(collector.coords, [
-      c(1, 2),
-      c(2, 2)
-    ]);
+      assert.deepEqual(collector.coords, [
+        c(1, 2),
+        c(2, 2)
+      ]);
+    });
+
+    it('finds cells touched by left-facing line', function() {
+      var start = p(22, 11);
+      var end = p(1, 12);
+
+      walker.walk(start, end, collector);
+
+      assert.deepEqual(collector.coords, [
+        c(2, 2),
+        c(1, 2)
+      ]);
+    });
+
+    it('finds lower cell when line runs along cell boundary', function() {
+      var start = p(22, 10);
+      var end = p(25, 10);
+
+      walker.walk(start, end, collector);
+
+      assert.deepEqual(collector.coords, [
+        c(2, 2)
+      ]);
+    });
   });
 
-  it('finds both cells when left-facing line crosses horizontally adjacent cells', function() {
-    var start = p(22, 11);
-    var end = p(1, 12);
+  describe('walking veritcal line', function () {
+    it('finds cells touched by down-facing line', function() {
+      var start = p(1, 1);
+      var end = p(1, 12);
 
-    walker.walk(start, end, collector);
+      walker.walk(start, end, collector);
 
-    assert.deepEqual(collector.coords, [
-      c(2, 2),
-      c(1, 2)
-    ]);
+      assert.deepEqual(collector.coords, [
+        c(1, 1),
+        c(1, 2)
+      ]);
+    });
+
+    it('finds cells touched by up-facing line', function() {
+      var start = p(1, 11);
+      var end = p(1, 1);
+
+      walker.walk(start, end, collector);
+
+      assert.deepEqual(collector.coords, [
+        c(1, 2),
+        c(1, 1)
+      ]);
+    });
+
+    it('finds right-hand cell when line runs along cell boundary', function() {
+      var start = p(20, 1);
+      var end = p(20, 9);
+
+      walker.walk(start, end, collector);
+
+      assert.deepEqual(collector.coords, [
+        c(2, 1)
+      ]);
+    });
   });
-
-  it('finds lower cell when horizontal line runs along cell boundary', function() {
-    var start = p(22, 10);
-    var end = p(25, 10);
-
-    walker.walk(start, end, collector);
-
-    assert.deepEqual(collector.coords, [
-      c(2, 2)
-    ]);
-  });
-
-  it('finds both cells when down-facing line cross vertically adjacent cells', function() {
-    var start = p(1, 1);
-    var end = p(1, 12);
-
-    walker.walk(start, end, collector);
-
-    assert.deepEqual(collector.coords, [
-      c(1, 1),
-      c(1, 2)
-    ]);
-  });
-
-  it('finds both cells when up-facing line cross vertically adjacent cells', function() {
-    var start = p(1, 11);
-    var end = p(1, 1);
-
-    walker.walk(start, end, collector);
-
-    assert.deepEqual(collector.coords, [
-      c(1, 2),
-      c(1, 1)
-    ]);
-  });
-
-  it('finds right-hand cell when vertical line runs along cell boundary', function() {
-    var start = p(20, 1);
-    var end = p(20, 9);
-
-    walker.walk(start, end, collector);
-
-    assert.deepEqual(collector.coords, [
-      c(2, 1)
-    ]);
-  });
-
-
 });
 
 // point in space
